d039708a9bf20468d225c8aad96be8b765f32e9d
Juliver Galleto <juliver.galleto@gmail.com>
2018-11-14 15:37:57 +0800

    adjustments

--- tourMe.js
+++ tourMe.js
@@ -254,6 +254,9 @@ function tourMe(){
         document.querySelector('#tourme-data').value = JSON.stringify($data);
 
       }else{
+        if( document.querySelector('.tourme-pointer') !== null ){
+           document.querySelector('.tourme-pointer').classList.remove('tourme-pointer');
+        }
         var $tourme_bg_el = document.querySelector('#tourme-bg');
         $tourme_bg_el.parentNode.removeChild($tourme_bg_el);
         var $tourme_content_el = document.querySelector('#tourme-content');
